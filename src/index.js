337a971759a97edffef9cc8ae9d915a59e4ba87a
Mykola Kunak <kunak.n@gmail.com>
2020-10-24 14:24:23 +0300

    mkunak>master: lintes prettier settings

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+
+let mainWindow;
+
+// Listen for app to be ready:
+app.on('ready', () => {});
